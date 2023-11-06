140bf9553b0b018a2b1fac66fd2b93e523fae383
sinned8 <dennisdust2@gmail.com>
2023-11-06 16:04:29 -0500

    Finsihed the main model JS added dateFNS to handel the due date and uuid for ID generation of tasks and used the local storage in the web pack api for storage.

--- src/sections/model/mainModel.js
+++ src/sections/model/mainModel.js
@@ -12,14 +12,60 @@ export default class MainModel
         this.projectList =  new ProjectList();
     }
 
-    createAndAddNewTaskItem(name,description,dueDate,project,checkboxStatus)
+    createAndAddNewTaskItem(name, description, dueDate, project, checkboxStatus)
     {
-        const newTaskItem = new TaskItem(name,description,dueDate,project,checkboxStatus)
+        const newTaskItem = new TaskItem(name, description, dueDate, project, checkboxStatus)
         this.taskList.add(newTaskItem)
 
         return newTaskItem
     }    
 
+    updateTaskItem(element, name, description, dueDate, project)
+    {
+        element.model.name = name;
+        element.model.description = description;
+        element.model.dueDate = dueDate;
+        element.model.project =project;
+        return element
+
+    }
+    removeItemById(id)
+    {
+        this.taskList.removeById(id)
+    }
 
 
+    createNewProject(name)
+    {
+        const newProject = new Project(name);
+        this.projectList.add(newProject);
+        return newProject;
+    }
+    removeProjectById(id)
+    {
+        this.projectList.removeById(id);
+    }
+    updateTaskListLocalStorage()
+    {
+        window.localStorage.setItem('taskList', JSON.stringify(this.taskList))
+    }
+    updateProjectListLocalStorage()
+    {
+        window.localStorage.setItem('projectList', JSON.stringify(this.projectList))
+    }
+    updateCheckBox(id, value)
+    {
+        this.taskList.getItemByID(id).checkboxStatus = value;
+    }
+
+    getTodayDate()
+    {
+        let today = new Date(); 
+
+        function addZ(n) {
+            return (n<10? '0' : '') + n;
+        }
+
+        return today.getFullYear() + '-' + addZ(today.getMonth() + 1) + '-' + addZ(today.getDate());
+    }
 }
