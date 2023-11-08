ff4fc6d56bf2e03535a75e158024b0763c380d9d
sinned8 <dennisdust2@gmail.com>
2023-11-08 13:27:24 -0500

    Added templates to be copied for projects and tasks

--- src/sections/view/mainView.js
+++ src/sections/view/mainView.js
@@ -1,2 +1,27 @@
 import TaskView from './taskView'
 import ProjectView from './projectView'
+
+export default class MainView{
+    getByID(id){
+        return document.getElementById(id)
+    }
+    toggleAddTaskFormVisibility(visible, id)
+    {
+        const displayValue = visible ? 'block' : 'none';
+        this.getByID(id).style.display = displayValue;
+    }
+
+    toggleFilterVisiblity(visible, id){
+        const displayValue = visible ? 'grid' : 'none';
+        this.getByID(id).style.display = displayValue
+    }
+
+    getAddProjectFormElementValueByName(elementName){
+        return this.getByID('add-proj-forum').elements[elementName].value
+    }
+
+    resetAndCloseFormByID(formID,formContainerID){
+        this.getByID(formID).reset()
+        this.toggleAddTaskFormVisibility(false,formContainerID)
+    }
+}
