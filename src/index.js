2b4e93814190936982096d2c011b6e0034df2d48
sinned8 <dennisdust2@gmail.com>
2023-11-17 14:09:23 -0500

    Possible fix for svgs not rendering on gh pages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/style.css'
 import MainModel from './sections/model/mainModel'
 import MainView from './sections/view/mainView'
 import MainController from './sections/maincontroller'
+import PencilOutline from './assests/imgs/pencil-outline.svg'
 
 const model = new MainModel()
 const view = new MainView()
